05ed787411adbeb266a955597c431f7b55cedc11
AshCherr96 <ashleycherry16@gmail.com>
2026-04-20 23:23:50 -0500

    Complete Lesson 3: Props and TodoListItem refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css';
+import React, { useState } from 'react'; // Import useState
 import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
 
+
+const todos = [
+  { id: 1, title: 'Complete assignment' },
+  { id: 2, title: 'Check class notes' },
+  { id: 3, title: 'Submit PR' },
+];
+
 function App() {
+  const [todoList, setTodoList] = useState(todos);
+
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm />
-      <TodoList />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
